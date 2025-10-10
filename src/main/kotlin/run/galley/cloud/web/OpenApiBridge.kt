a295e5bca333b334e6ac2dcce4fcc2dcf49efbc7
Patrick van Marsbergen <patrick@vanmarsbergen.com>
2025-10-10 15:00:20 +0200

    Fix lint

--- src/main/kotlin/run/galley/cloud/web/OpenApiBridge.kt
+++ src/main/kotlin/run/galley/cloud/web/OpenApiBridge.kt
@@ -93,8 +93,9 @@ class OpenApiBridge(
             val charterIds = routingContext.user().principal().getJsonArray("charterIds")
 
             // Check if user has access to the charter (charter ids should be in JWT, added from table crew_charter_member)
-            val hasCharterAccess = requestedCharterId != null && charterIds != null &&
-              charterIds.toList().map { it?.toString() }.contains(requestedCharterId)
+            val hasCharterAccess =
+              requestedCharterId != null && charterIds != null &&
+                charterIds.toList().map { it?.toString() }.contains(requestedCharterId)
 
             if (hasCharterAccess) {
               // User has access to this charter

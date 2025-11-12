bfbfaf19006897ad648e81eee56e7f15f68087bc
Patrick van Marsbergen <patrick@vanmarsbergen.com>
2025-11-12 13:10:42 +0100

    Code Quality fixes

--- src/main/kotlin/nl/clicqo/eventbus/EventBusCmdDataRequest.kt
+++ src/main/kotlin/nl/clicqo/eventbus/EventBusCmdDataRequest.kt
@@ -1,15 +1,13 @@
 package nl.clicqo.eventbus
 
 import io.vertx.core.json.JsonObject
-import run.galley.cloud.model.BaseModel
 import java.util.UUID
 
 /**
  * Represents a command data request for EventBus operations, typically used to manage or modify
  * resources in the system. Supports specifying identifiers, filters, user context, and data payloads.
  *
- * @param T The type of the model extending [BaseModel] which this request works with.
- * @property payload Encapsulates the data payload, which can include a single or multiple items of type [T].
+ * @property payload Encapsulates the data payload as JsonObject
  * @property identifier Optional unique identifier for updating existing resources.
  * @property filters A map of filtering criteria, useful for targeting specific resources.
  * @property userId The ID of the user initiating the request, providing context for auditing or attribution.
